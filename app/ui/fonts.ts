e992ed129ad6749d1a95fe2185e29647182744d3
GauranshMathur <40708506+GauranshMathur@users.noreply.github.com>
2024-07-19 00:35:35 +0800

    feat: Add global CSS and font imports
    This commit adds global CSS and font imports to improve the styling and typography across the application. The `global.css` file is imported in the `layout.tsx` file, and the `fonts.ts` file is added to handle the font imports. This will ensure consistent styling and typography throughout the application.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({subsets: ['latin']});
+
+
+export const subFontLusitana = Lusitana({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'] 
+});
